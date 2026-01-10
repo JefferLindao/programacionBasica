0fd490b32635bc29207d35ba2b283b9a9471d273
JefferLindao <34347800+JefferLindao@users.noreply.github.com>
2026-01-09 23:40:52 -0500

    renderizacion dinamico de objeto en html con javascript

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -16,10 +16,12 @@ const spanVidasEnemigo = document.getElementById('vidas-enemigo')
 const sectionMensajes = document.getElementById('resultado')
 const ataqueDelJugador = document.getElementById('ataque-del-jugador');
 const ataqueDelEnemigo = document.getElementById('ataque-del-enemigo');
+const contenedorTarjetas = document.getElementById('contenedor-tarjetas');
 
 let mokepones = []
 let ataqueJugador
 let ataqueEnemigo
+let opcionDeMokepones
 let vidasJugador = 3
 let vidasEnemigo = 3
 
@@ -60,8 +62,19 @@ ratigueya.ataques.push(
     {nombre: '🌱', id: 'boton-tierra'},
 )
 
+mokepones.push(hipodoge,capipepo,ratigueya)
+
 function iniciarJuego() {
     sectionSeleccionarAtaque.style.display = 'none'
+    mokepones.forEach(mokepon => {
+        opcionDeMokepones = `<input type="radio" name="mascota" id=${mokepon.nombre} />
+      <label class="tarjeta-de-mokepon" for=${mokepon.nombre}>
+        <p>${mokepon.nombre}</p>
+        <img src=${mokepon.foto} alt=${mokepon.nombre}>
+      </label>
+        `
+        contenedorTarjetas.innerHTML += opcionDeMokepones
+    })
     sectionReiniciar.style.display = 'none'
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
     botonFuego.addEventListener('click', ataqueFuego)
