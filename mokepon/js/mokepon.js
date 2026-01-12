3df9a74a4d53b1e33e3503b081a6149486494146
JefferLindao <34347800+JefferLindao@users.noreply.github.com>
2026-01-11 21:32:42 -0500

    secuencia de ataque y validacion de combate en juego javascript

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -15,13 +15,14 @@ const contenedorAtaques = document.getElementById('contenedor-ataques');
 
 let mokepones = []
 let ataqueJugador= []
-let ataqueEnemigo
+let ataqueEnemigo=[]
 let opcionDeMokepones
 let inputHipodoge
 let inputCapipepo
 let inputRatigueya
 let mascotaJugador
 let ataquesMokepon
+let ataqueMokeponEnemigo = []
 let botonFuego
 let botonAgua
 let botonTierra
@@ -147,6 +148,7 @@ function secuenciaAtaque() {
                 console.log(ataqueJugador)
                 boton.style.background = '#112f58'
             }
+            ataqueAleatorioEnemigo()
         })
     })
 }
@@ -155,19 +157,20 @@ function seleccionarMascotaEnemigo() {
     let mascotaAleatoria = aleatorio(0, mokepones.length - 1)
 
     spanMascotaEnemigo.innerHTML = mokepones[mascotaAleatoria].nombre
+    ataqueMokeponEnemigo = mokepones[mascotaAleatoria].ataques
     secuenciaAtaque()
 }
 
 function ataqueAleatorioEnemigo() {
-    let ataqueAleatorio = aleatorio(1, 3)
-    if (ataqueAleatorio == 1) {
-        ataqueEnemigo = 'FUEGO'
-    } else if (ataqueAleatorio == 2) {
-        ataqueEnemigo = 'AGUA'
+    let ataqueAleatorio = aleatorio(0, ataqueMokeponEnemigo.length - 1)
+    if (ataqueAleatorio === 0 || ataqueAleatorio === 1) {
+        ataqueEnemigo.push('FUEGO')
+    } else if (ataqueAleatorio === 3 || ataqueAleatorio === 4) {
+        ataqueEnemigo.push('AGUA')
     } else {
-        ataqueEnemigo = 'TIERRA'
+        ataqueEnemigo.push('TIERRA')
     }
-
+    console.log(ataqueEnemigo)
     combate()
 }
 
